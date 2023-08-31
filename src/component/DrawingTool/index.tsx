f293cbdce23ef08d8fa24a667a8a0455c69856a2
openkmj <openkmj@gmail.com>
2023-09-01 00:34:23 +0900

    define color set

--- src/component/DrawingTool/index.tsx
+++ src/component/DrawingTool/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
+import { colorSet } from "../../util/color";
 import styles from "./index.module.css";
-
-const colorSet = [];
 
 interface DrawingToolProps {
   canvasManager: any;
@@ -11,7 +10,11 @@ const DrawingTool = ({ canvasManager }: DrawingToolProps) => {
   return (
     <div className={styles.drawingTool}>
       <div></div>
-      <div>{colorSet.map((i) => {})}</div>
+      <div>
+        {colorSet.map((i) => {
+          return <div>{i.hex}</div>;
+        })}
+      </div>
     </div>
   );
 };
---
+++ src/util/color.ts
@@ -0,0 +1,83 @@
+export const colorSet = [
+  {
+    // white
+    hex: "#FFFFFF",
+    rgb: [255, 255, 255],
+  },
+  {
+    // black
+    hex: "#000000",
+    rgb: [0, 0, 0],
+  },
+  {
+    // red
+    hex: "#FF4343",
+    rgb: [255, 67, 67],
+  },
+  {
+    // orange
+    hex: "#FF8743",
+    rgb: [255, 135, 67],
+  },
+  {
+    // yellow
+    hex: "#FFE143",
+    rgb: [255, 225, 67],
+  },
+  {
+    // light green
+    hex: "#8AFF43",
+    rgb: [138, 255, 67],
+  },
+  {
+    // green
+    hex: "#3FCC33",
+    rgb: [63, 204, 51],
+  },
+  {
+    // blue
+    hex: "#3864FF",
+    rgb: [56, 100, 255],
+  },
+  {
+    // dark blue
+    hex: "#002DCB",
+    rgb: [0, 45, 203],
+  },
+  {
+    // purple
+    hex: "#8A43FF",
+    rgb: [138, 67, 255],
+  },
+  {
+    // pink
+    hex: "#FF43BF",
+    rgb: [255, 67, 191],
+  },
+  {
+    // grey
+    hex: "#C8C8C8",
+    rgb: [200, 200, 200],
+  },
+  // TODO: you can add 4 colors here
+  {
+    // black
+    hex: "#000000",
+    rgb: [0, 0, 0],
+  },
+  {
+    // black
+    hex: "#000000",
+    rgb: [0, 0, 0],
+  },
+  {
+    // black
+    hex: "#000000",
+    rgb: [0, 0, 0],
+  },
+  {
+    // black
+    hex: "#000000",
+    rgb: [0, 0, 0],
+  },
+];
